0d7e835a1d3f337012082c0289d6451fe3261a4c
mrri <marouane.ourdou@edu.uiz.ac.ma>
2021-02-16 21:19:02 +0100

    added some styles

--- app.js
+++ app.js
@@ -2,7 +2,38 @@ const app = Vue.createApp({
     data() {
         return {
             header: "hello, world",
-            products: [1, 2, 3, 4]
+            products: [
+                {
+                    image: "./assets/product.jpeg",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nostrum. Id deleniti saepe odio",
+                    price: 15.98,
+                    reduction: 17.56
+                },
+                {
+                    image: "./assets/product1.jpeg",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nostrum. Id deleniti saepe odio",
+                    price: 15.98,
+                    reduction: 17.56
+                },
+                {
+                    image: "./assets/product2.jpeg",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nostrum. Id deleniti saepe odio",
+                    price: 15.98,
+                    reduction: 17.56
+                },
+                {
+                    image: "./assets/product.jpeg",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nostrum. Id deleniti saepe odio",
+                    price: 15.98,
+                    reduction: 17.56
+                },
+                {
+                    image: "./assets/product2.jpeg",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nostrum. Id deleniti saepe odio",
+                    price: 15.98,
+                    reduction: 17.56
+                }
+            ]
         }
     }
 })
